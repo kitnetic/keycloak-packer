60abd4957e8c4c884986a7e1bb208293ea0f8378
Thomas Remmert <>
2020-12-30 21:49:14 +0000

    switching to S3 Ping

--- install-database-drivers.sh
+++ install-database-drivers.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-sudo mkdir -p /opt/keycloak-10.0.2/modules/system/layers/keycloak/org/mariadb/main
-sudo wget https://dlm.mariadb.com/1484707/Connectors/java/connector-java-2.7.1/mariadb-java-client-2.7.1.jar /tmp
-sudo mv /tmp/mariadb-java-client-2.7.1.jar /opt/keycloak-10.0.2/modules/system/layers/keycloak/org/mariadb/main/.
+mkdir -p /opt/keycloak/modules/system/layers/keycloak/org/mariadb/main
+wget https://dlm.mariadb.com/1484707/Connectors/java/connector-java-2.7.1/mariadb-java-client-2.7.1.jar -P /tmp
+mv /tmp/mariadb-java-client-2.7.1.jar /opt/keycloak/modules/system/layers/keycloak/org/mariadb/main/.
 
+mv module.xml /opt/keycloak/modules/system/layers/keycloak/org/mariadb/main/module.xml
 
